ae37000cdf1aafc2e733f13497918b398ccc8b6a
Putri Natali Br. Situmorang <96192596+PutriNataliSitumorang@users.noreply.github.com>
2022-11-22 13:52:53 +0700

    Add files via upload

---
+++ ProjectUts.java
@@ -0,0 +1,57 @@
+/* * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectuts;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+
+  class Ukulele{
+      
+ String Merk;
+ String WarnaUkulele;
+ String TypeUkulele;
+ String UkuranUkulele;}
+
+public class ProjectUts {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Ukulele ukuleleku = new Ukulele();
+        ukuleleku.  Merk =  " Mandalika ";
+        ukuleleku.  WarnaUkulele = " Coklat ";
+        ukuleleku.  TypeUkulele = " Sopran ";
+        ukuleleku.  UkuranUkulele = " 21 CM ";
+        
+        System.out.println("Merk" + ukuleleku. Merk);
+        System.out.println("Warna" + ukuleleku. WarnaUkulele);
+        System.out.println("TypeUkuelele" + ukuleleku. TypeUkulele);
+        System.out.println("UkuranUkulele" + ukuleleku. UkuranUkulele);
+        
+    }
+
+        
+}
+     
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+ 
+    
+    
